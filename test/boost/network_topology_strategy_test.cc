scylladb/scylladb

mv: handle mismatched base/view replica count caused by RF change
During an ALTER KEYSPACE statement execution where a table with a view
is present, we need to perform tablet migrations for both tables.
These migrations are not synchronized, so at some point the base may
have a different number of non-pending replicas than the view. Because
of that, we can't pair them correctly. If there is more non-pending
base replicas than view replicas, we don't need to do anything because
the view replica that didn't finish migrating is a pending replica
and will get view updates from all base replicas. But if there is more
non-pending view replicas than base replicas, we may currently lose
view updates to the new view replica.

This patch adds a workaround for this scenario. If after one migration
we have too more non-pending view replicas than base replicas, we add
it to the pending replica list so that it gets an update anyway.

This patch will also take effect if the base and view replica counts
differ due to some other bug. To track that, a new metric is added
to count such occurrences.

This patch also includes a test for this exact scenario, which is enforced by an injection.

Fixes https://github.com/scylladb/scylladb/issues/21492

test/boost/network_topology_strategy_test.cc
@@ -1333,7 +1333,7 @@ SEASTAR_THREAD_TEST_CASE(tablets_simple_rack_aware_view_pairing_test) {
             view_token,
             use_legacy_self_pairing,
             use_tablets_basic_rack_aware_view_pairing,
-            cf_stats);
+            cf_stats).first;
 
         // view pair must be found
         BOOST_REQUIRE(view_ep_opt);
@@ -1488,7 +1488,7 @@ void test_complex_rack_aware_view_pairing_test(bool more_or_less) {
             view_token,
             use_legacy_self_pairing,
             use_tablets_basic_rack_aware_view_pairing,
-            cf_stats);
+            cf_stats).first;
 
         // view pair must be found
         if (!view_ep_opt) {

wmitros/scylla

test/boost/network_topology_strategy_test.cc
@@ -1342,7 +1342,7 @@ SEASTAR_THREAD_TEST_CASE(tablets_simple_rack_aware_view_pairing_test) {
             view_token,
             use_legacy_self_pairing,
             use_tablets_basic_rack_aware_view_pairing,
-            cf_stats);
+            cf_stats).natural_endpoint;
 
         // view pair must be found
         BOOST_REQUIRE(view_ep_opt);
@@ -1497,7 +1497,7 @@ void test_complex_rack_aware_view_pairing_test(bool more_or_less) {
             view_token,
             use_legacy_self_pairing,
             use_tablets_basic_rack_aware_view_pairing,
-            cf_stats);
+            cf_stats).natural_endpoint;
 
         // view pair must be found
         if (!view_ep_opt) {